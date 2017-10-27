f0d12ca9cf5acc8a822dce7d83baa9f58f71effd
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-26 17:30:23 -0700

    optimized using while loop

--- wk1Challenges/dictionary_words.py
+++ wk1Challenges/dictionary_words.py
@@ -5,30 +5,34 @@ def load_word():
     """Read the usr/shared/dict/words file to get a list of words."""
     with open("/usr/share/dict/words", "r") as f:
         words_list = f.readlines()
-        # striped_words = [item.strip() for item in words_list]
         random_word = random.choice(words_list)
         return random_word.strip()
 
 
 def construct_sentence(num):
     """Build a sentence with random words from load_word() func."""
-    sentence = ""
+    # sentence = ""
+    sentence = []
     while len(sentence) < num:
-        sentence += (load_word() + " ")
-    print(sentence)
+    # for index in range(0, num):
+        # sentence += (load_word() + " ")
+        sentence.append(load_word())
+    # print(sentence)
+    joined = " ".join(sentence)
+    return (joined + ".")
 
 
-time2 = time.time()
-print("construct time: " + str(time2))
+# time2 = time.time()
+# print("construct time: " + str(time2))
 
 
 if __name__ == '__main__':
     time1 = time.time()
-    print("load time: " + str(time1))
+    # print("load time: " + str(time1))
     words = open("/usr/share/dict/words", "r").readlines()
-    params = sys.argv[1:]
-    num = int(params[0])
+    params = sys.argv[1]
+    num = int(params)
     print(construct_sentence(num))
     time2 = time.time()
-    print("run program time: " + str(time2))
+    # print("run program time: " + str(time2))
     print(time1 - time2)
