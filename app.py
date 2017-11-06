016bbb2341ef88bd15c46e90e90608d726518705
Phyllis Wong <wong.phyllism@gmail.com>
2017-11-06 11:17:53 -0800

    comment out the sqlalchemy

--- app.py
+++ app.py
@@ -11,23 +11,22 @@ import sample as s
 app = Flask(__name__)
 
 # MIDDLEWARE
-if os.environ.get('DATABASE_URL'):
-    db_url = os.environ.get('DATABASE_URL')
-else:
-    db_url = 'postgresql://localhost/TweetGenerator'
+# if os.environ.get('DATABASE_URL'):
+#     db_url = os.environ.get('DATABASE_URL')
+# else:
+#     db_url = 'postgresql://localhost/TweetGenerator'
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+#
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+#
+# engine = create_engine('postgresql://localhost/TweetGenerator')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-engine = create_engine('postgresql://localhost/TweetGenerator')
 
 # ROUTES
-
-
 @app.route('/')
-def hello():
+def rand_sentence():
     dictionary = s.create_dict_from_file()
     # rand_wrd = get_random_wrd(dictionary)
     dict_w_weights = s.calculate_probability(dictionary)
