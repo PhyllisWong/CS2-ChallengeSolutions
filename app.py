7663607bdbd4ddd386e55858b5af8cea0e061120
Phyllis Wong <wong.phyllism@gmail.com>
2017-11-06 10:38:36 -0800

    fix flask env variables

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
+
+# DELCARATIONS
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[TweetGenerator]'
-db = SQLAlchemy(app)
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+import os
+
 import sample as s
 # import histogram as h
+
 app = Flask(__name__)
 
+# MIDDLEWARE
 
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://localhost/[YOUR_DATABASE_NAME]')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'postgresql://localhost/TweetGenerator'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+engine = create_engine('postgresql://localhost/TweetGenerator')
+
+# ROUTES
 
 
 @app.route('/')
