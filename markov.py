30fe5a3dfd24b92d2c4c10edf0d459d75efee941
Phyllis Wong <wong.phyllism@gmail.com>
2017-11-14 18:01:42 -0800

    added method stubs

---
+++ markov.py
@@ -0,0 +1,39 @@
+#!python
+from dictogram import Dictogram
+import sentence
+import sample
+import random
+from __future__ import division, print_function  # Python 2 and 3 compatibility
+
+
+class Markov(dict):
+    """Dictogram is a histogram implemented as a subclass of the dict type."""
+
+    def __init__(self, word_list):
+        """"""
+
+    def check_word_after_word(self, word_list):
+        pass
+
+    def create_states(self, word_list):
+        pass
+
+    def create_states_lists_counts(self):
+        pass
+
+    def calculate_probabilities_dict(self):
+        pass
+
+    def pick_word(self, word):
+        pass
+
+    def pick_many_words(self, number_of_words):
+        pass
+
+    
+    def main():
+        pass
+
+
+if __name__ == '__main__':
+    main()
