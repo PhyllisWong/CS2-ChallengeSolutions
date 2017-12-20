03d857d4d738e7dd9313f397d019febc7a815ea5
Phyllis Wong <wong.phyllism@gmail.com>
2017-12-19 18:28:05 -0800

    clean up sentence constructor module

--- sentence_constructor.py
+++ sentence_constructor.py
@@ -4,7 +4,6 @@ import random, sys, re
 import markov as m
 from pprint import pprint
 from collections import deque
-# import sys, string, re, time coming from the histogram file
 
 
 def create_dict_from_list(clean_list):
@@ -15,10 +14,8 @@ def create_dict_from_list(clean_list):
     for index in range(0, len(clean_list)-1):
         cur = clean_list[index]
         nxt = clean_list[index+1]
-        # print("iterator: {}".format(index))
         list_of_pairs.append((cur, nxt))
     dictionary = h.histogram_dict(list_of_pairs)
-    # print(dictionary)
     return dictionary
 
 
@@ -39,7 +36,6 @@ def calculate_probability(dictionary):
 
     for (wrd, value) in dictionary.items():
         weight = float(value / total_tokens)
-        # Set the value to the weight
         dict_w_weights[wrd] = weight
     del dict_w_weights[('dream', 'STOP')]
     return dict_w_weights
@@ -53,7 +49,6 @@ def get_random_tuple_prob(dict_w_weights):
         probability += wrd_weight
         if rand_float < probability:
             break
-    # print(dict_w_weights)
     return tpl
 
 
@@ -70,12 +65,8 @@ def get_many_rand_wrds(dictionary, num):
 
 
 def find_wrd_after_tuple_key(tuple_key, markov_dict):
-    # print("USE THIS Tuple: {}\n".format(tuple_key))
-    # print("SECOND ORDER markov: {}\n".format(markov_dict))
     histogram = markov_dict.get(tuple_key)
-    # print("histogram matching the tuple: {}\n".format(histogram))
     nxt_rand_wrd = get_random_wrd(histogram)
-    # print("line 96 {}".format(nxt_rand_wrd))
     return nxt_rand_wrd
 
 
@@ -86,7 +77,6 @@ def find_word_after_rand_wrd(rand_wrd, markov_dict):
         if types == rand_wrd:
             histogram = markov_dict[types]
             if len(histogram) > 1:
-                # for (k, v) in histogram.items():
                 nxt_rand_wrd = get_random_wrd(histogram)
                 return nxt_rand_wrd
             else:
@@ -100,23 +90,18 @@ def create_sentence(wrd_num, dict_w_weights, markov_dict):
     Take in num of words in sentence, and histogram. Return a sentence.'''
     sentence = []
     rand_tuple = get_random_tuple_prob(dict_w_weights)
-    # print('rand_tuple:', rand_tuple)
     second_to_last = rand_tuple[0]
     last_wrd = rand_tuple[1]
     sentence.append(second_to_last)
     sentence.append(last_wrd)
     nxt_wrd = find_wrd_after_tuple_key(rand_tuple, markov_dict)
     while len(sentence) < wrd_num:
-        # print('sentence:', sentence)
         rand_tuple = (last_wrd, nxt_wrd)
         second_to_last = last_wrd
         last_wrd = nxt_wrd
-        # if nxt_nxt_wrd is None:
-        # print('nxt_wrd:', nxt_wrd)
         if nxt_wrd != 'STOP':
             sentence.append(nxt_wrd)
             nxt_wrd = find_wrd_after_tuple_key(rand_tuple, markov_dict)
-            # print('rand_tuple:', rand_tuple)
         else:
             break
     joined = " ".join(sentence) + "."
@@ -134,21 +119,10 @@ clean_list = clean_text()
 
 def construct_sentence(wrd_num, clean_list):
     markov_dict = m.second_order_markov_chain(clean_list)
-    # print("Second order markov chain:")
-    # pprint(markov_dict)
-
     dictionary = create_dict_from_list(clean_list)
-    # print("Dictionary of tuples:")
-    # pprint(dictionary)
-
     dict_w_weights = calculate_probability(dictionary)
-    # print("Dictionary with weights:")
-    # pprint(dict_w_weights)
-
     rand_sentence = create_sentence(wrd_num, dict_w_weights, markov_dict)
-    # print("This is my sentence: {}".format(rand_sentence))
     tweet = limit_140_chars(rand_sentence)
-    # return rand_sentence
     print(tweet)
     return tweet
 
@@ -163,15 +137,9 @@ def limit_140_chars(rand_sentence):
     tweet = re.sub("i'd", "I'd", tweet)
     tweet = re.sub("does n", "doesn't", tweet)
     tweet = re.sub("doesn t", "doesn't", tweet)
-    # print(tweet)
     return tweet
 
 
 if __name__ == '__main__':
     clean_list = clean_text()
     construct_sentence(18, clean_list)
-
-
-
-    # Proof that many random words returns each word within the desired range
-    # get_many_rand_wrds(dict_w_weights, usr_input_count)
